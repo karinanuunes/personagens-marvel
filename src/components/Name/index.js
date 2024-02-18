2126a577234d5484991179cb9ea1ccb47de343f0
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-02-17 23:42:47 -0300

    change name

---
+++ src/components/Name/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+const Name = (props) => {
+  return <h2>{props.name}</h2>;
+};
+
+export default Name;
--- src/components/Title/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./styles.css";
-
-const Title = (props) => {
-  return <h2>{props.title}</h2>;
-};
-
-export default Title;
