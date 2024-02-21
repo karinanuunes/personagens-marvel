e2881e4fd7ce12d8133475441a189b7039c9fc2f
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-02-21 11:51:29 -0300

    att styles

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,5 +1,5 @@
 import { useNavigate, useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import "./details.css";
 
 // import Lista from "../../List"
@@ -13,50 +13,82 @@ function Details() {
     navigate("/");
   }
 
-  const [classe, setClasse] = useState("");
-  const handleClass = () => {
-    if (character.tipo === "Herói") {
-      setClasse("cgreen");
-    } else if (character.tipo === "Vilão") {
-      setClasse("cred");
-    }
-  };
-  useEffect(handleClass, [character.tipo]);
+  // const [classe, setClasse] = useState("");
+  // const handleClass = () => {
+  //   if (character.tipo === "Herói") {
+  //     setClasse("cgreen");
+  //   } else if (character.tipo === "Vilão") {
+  //     setClasse("cred");
+  //   }
+  // };
+  // useEffect(handleClass, [character.tipo]);
 
   // const Parag = ({ poder }) => {
   //   return <p>{poder}</p>
   // }
 
   return (
-    <div className="cp-bg-img">
-      <div className="cp-opacity">
-        <div className={`char-page-container ${classe}`}>
-          <h1 className="charpage-name">{character.nome}</h1>
-          <img
-            className="charpage-img"
-            src={character.urlImagem}
-            alt="character"
-          />
-          <div className="data-container">
-            <h2>Tipo:</h2>
-            <p>{character.tipo}</p>
-            <h2>Raca:</h2>
-            <p>{character.raca}</p>
-            {/* <h2>Poderes:</h2>
-            {character.poderes.map((e, index) => <Parag key={index} poder={e} />)} */}
-            <h2>Historia:</h2>
-            <p>{character.descricao}</p>
-            <h2>Idade:</h2>
-            <p>{character.idade}</p>
-            <h2>Altura:</h2>
-            <p>{character.altura}</p>
-          </div>
+    // <div className="cp-bg-img">
+    //   <div className="cp-opacity">
+    //     <div className={`char-page-container ${classe}`}>
+    //       <h1 className="charpage-name">{character.nome}</h1>
+    //       <img
+    //         className="charpage-img"
+    //         src={character.urlImagem}
+    //         alt="character"
+    //       />
+    //       <div className="data-container">
+    //         <h2>Tipo:</h2>
+    //         <p>{character.tipo}</p>
+    //         <h2>Raca:</h2>
+    //         <p>{character.raca}</p>
+    //         {/* <h2>Poderes:</h2>
+    //         {character.poderes.map((e, index) => <Parag key={index} poder={e} />)} */}
+    //         <h2>Historia:</h2>
+    //         <p>{character.descricao}</p>
+    //         <h2>Idade:</h2>
+    //         <p>{character.idade}</p>
+    //         <h2>Altura:</h2>
+    //         <p>{character.altura}</p>
+    //       </div>
 
-          <button className="go-home-button" onClick={goToHome}>
-            Home
-          </button>
+    //       <button className="go-home-button" onClick={goToHome}>
+    //         Home
+    //       </button>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="details-card">
+      <div className="image-card">
+        <img src={character.urlImagem} alt={character.nome} />
+      </div>
+      <div className="tipo-card">
+        <p>{character.tipo}</p>
+      </div>
+      <h2 className="nome-card">{character.nome}</h2>
+      <p className="descricao-card">{character.descricao}</p>
+
+      <div class="caracteristicas">
+        <div class="itens">
+          <div class="caracteristica-card-valor">
+            <p>{character.idade}</p>
+          </div>
+          <div class="caracteristica-card-item">Idade</div>
+        </div>
+
+        <div class="itens">
+          <div class="caracteristica-card-valor">{character.raca}</div>
+          <div class="caracteristica-card-item">Raça</div>
+        </div>
+
+        <div class="itens no-border">
+          <div class="caracteristica-card-valor">{character.altura}</div>
+          <div class="caracteristica-card-item">Altura</div>
         </div>
       </div>
+      <button onClick={goToHome} className="button-card">
+        Home
+      </button>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -36,74 +36,75 @@ function Home() {
   return (
     <div className="App">
       <Form onSubmit={handleFormSubmit} />
+      <div className="cards">
+        {personagens.map((personagem, index) => (
+          <Card
+            key={index}
+            character={personagem}
+            name={personagem.nome}
+            src={personagem.urlImagem}
+            alt={personagem.nome}
+            height={personagem.altura}
+            age={personagem.idade}
+            origin={personagem.origem}
+            race={personagem.raca}
+            type={personagem.tipo}
+            description={personagem.descricao}
+            // onDetail={verDetalhes}
+            detail="Ver detalhes"
+            onDelete={() => {
+              onDelete(index);
+            }}
+            delete="Deletar"
+          />
+        ))}
 
-      {personagens.map((personagem, index) => (
         <Card
-          key={index}
-          character={personagem}
-          name={personagem.nome}
-          src={personagem.urlImagem}
-          alt={personagem.nome}
-          height={personagem.altura}
-          age={personagem.idade}
-          origin={personagem.origem}
-          race={personagem.raca}
-          type={personagem.tipo}
-          description={personagem.descricao}
+          name="Homem Aranha"
+          src="https://cdn.awsli.com.br/600x700/1610/1610163/produto/177684974/poster-o-espetacular-homem-aranha-2-g-ebc6cbb4.jpg"
+          alt="Homem Aranha"
+          height={1.8}
+          age={15}
+          origin="Estados Unidos"
+          race="Humano"
+          type={hero ? "Herói" : "Vilão"}
+          description="É homem e aranha"
           // onDetail={verDetalhes}
           detail="Ver detalhes"
-          onDelete={() => {
-            onDelete(index);
-          }}
+          onDelete={onDelete}
           delete="Deletar"
         />
-      ))}
-
-      <Card
-        name="Homem Aranha"
-        src="https://cdn.awsli.com.br/600x700/1610/1610163/produto/177684974/poster-o-espetacular-homem-aranha-2-g-ebc6cbb4.jpg"
-        alt="Homem Aranha"
-        height={1.8}
-        age={15}
-        origin="Estados Unidos"
-        race="Humano"
-        type={hero ? "Herói" : "Vilão"}
-        description="É homem e aranha"
-        // onDetail={verDetalhes}
-        detail="Ver detalhes"
-        onDelete={onDelete}
-        delete="Deletar"
-      />
-      <Card
-        name="Capitão América"
-        src="https://i.pinimg.com/564x/91/00/7d/91007d0fe4e7ba58b213051e539dc70c.jpg"
-        alt="Capitão América"
-        height={1.9}
-        age={38}
-        origin="Estados Unidos"
-        race="Humano"
-        type={hero ? "Herói" : "Vilão"}
-        description="Velho descongelado"
-        // onDetail={verDetalhes}
-        detail="Ver detalhes"
-        onDelete={onDelete}
-        delete="Deletar"
-      />
-      <Card
-        name="Homem de Ferro"
-        src="https://i.pinimg.com/564x/52/08/ac/5208ac301eb3fb378dc6b69a5e94c6ec.jpg"
-        alt="Homem de Ferro"
-        height={1.85}
-        age={53}
-        origin="Estados Unidos"
-        race="Humano"
-        type={hero ? "Herói" : "Vilão"}
-        description="Velho rico"
-        // onDetail={verDetalhes}
-        detail="Ver detalhes"
-        onDelete={onDelete}
-        delete="Deletar"
-      />
+        <Card
+          name="Capitão América"
+          src="https://i.pinimg.com/564x/91/00/7d/91007d0fe4e7ba58b213051e539dc70c.jpg"
+          alt="Capitão América"
+          height={1.9}
+          age={38}
+          origin="Estados Unidos"
+          race="Humano"
+          type={hero ? "Herói" : "Vilão"}
+          description="Velho descongelado"
+          // onDetail={verDetalhes}
+          detail="Ver detalhes"
+          onDelete={onDelete}
+          delete="Deletar"
+        />
+        <Card
+          name="Homem de Ferro"
+          src="https://i.pinimg.com/564x/52/08/ac/5208ac301eb3fb378dc6b69a5e94c6ec.jpg"
+          alt="Homem de Ferro"
+          height={1.85}
+          age={53}
+          origin="Estados Unidos"
+          race="Humano"
+          type={hero ? "Herói" : "Vilão"}
+          description="Velho rico"
+          // onDetail={verDetalhes}
+          detail="Ver detalhes"
+          onDelete={onDelete}
+          delete="Deletar"
+        />
+      </div>
     </div>
   );
 }
